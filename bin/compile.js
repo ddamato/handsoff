ea7f3ab2c780a964a6b637724739d990d7f71e58
Donnie D'Amato <fauxserious@gmail.com>
2020-01-21 12:21:15 -0500

    Refactor, write database

--- bin/compile.js
+++ bin/compile.js
@@ -4,14 +4,8 @@ import glob from 'glob';
 
 const HTML_WEB_DIR = path.resolve(__dirname, '..', 'web');
 const HTML_DIST_DIR = path.resolve(__dirname, '..', 'dist');
+const DATABASE_JSON_PATH = path.resolve(__dirname, '..', 'gen', 'database.json');
 const TEMPLATE_TAG_REGEX = /{{\s*([\w\.]+)\s*}}/g;
-
-const existingContents = {
-  content: {
-    value: 'Hello world',
-    type: 'text',
-  } 
-};
 
 async function getHtmlFilePaths(directory) {
   return await glob.sync(`${directory}/**/*.html`);
@@ -21,39 +15,67 @@ async function getFileContents(path) {
   return await fs.readFileSync(path, 'utf-8');
 }
 
-function htmlTagManager(match, tagName) {
-  if (!existingContents[tagName]) {
-    existingContents[tagName] = { value: match, type: 'text' };
-  }
-  return existingContents[tagName].value;
-}
-
 async function writeFile(filePath, fileContents) {
   await fs.ensureFileSync(filePath);
   await fs.writeFileSync(filePath, fileContents, 'utf-8');
 }
 
+function initTag(value) {
+  return {
+    value,
+    type: 'text',
+    pages: [],
+  };
+}
+
 async function compile() {
+  let database = {};
+  try {
+    // Get the existing data
+    database = require(DATABASE_JSON_PATH);
+    console.info(`Reading: ${DATABASE_JSON_PATH}`);
+  } catch (err) {
+    // First time running; build the database later
+    console.info(`Building: ${DATABASE_JSON_PATH}`);
+  }
+
   // Get the .html files from the /web directory
   const htmlFilePaths = await getHtmlFilePaths(HTML_WEB_DIR);
   
   // For each path, create a promise
   const filePromises = htmlFilePaths.map(async (htmlPath) => {
+    // Create new file path
+    const newFilePath = htmlPath.replace(HTML_WEB_DIR, '');
+
     // Read the contents of the .html file
     const templatedHtml = await getFileContents(htmlPath);
 
     // Search for tags in the string, replace with content
-    const replacedHtml = templatedHtml.replace(TEMPLATE_TAG_REGEX, htmlTagManager);
+    const replacedHtml = templatedHtml.replace(TEMPLATE_TAG_REGEX, (match, tagName) => {
 
-    // Recreate the folder structure in dist
-    const newFilePath = HTML_DIST_DIR + htmlPath.replace(HTML_WEB_DIR, '');
+      // If the tag isn't in the database, init it
+      if (!database[tagName]) {
+        database[tagName] = initTag(match);
+      }
+      
+      // If the page isn't listed in the pages key, add it
+      const { pages } = database[tagName];
+      Array.isArray(pages) && !~pages.indexOf(newFilePath) && pages.push(newFilePath);
+
+      // Return the replacement value
+      return database[tagName].value;
+    });
 
     // Write the file
-    writeFile(newFilePath, replacedHtml);
+    writeFile(HTML_DIST_DIR + newFilePath, replacedHtml);
 
   });
 
+  // Ensure all the files have been processed
   await Promise.all(filePromises);
+
+  // Save updated database
+  writeFile(DATABASE_JSON_PATH, JSON.stringify(database));
 }
 
 compile();
